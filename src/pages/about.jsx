00bcb7f070047c420ff5570ed1753f7fe5c37c5e
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-07-26 01:23:28 -0500

    about me page info

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,19 +1,43 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet } from "react-router-dom";
 import profilePhoto from "../../assets/profile-photo.jpg";
 
 function AboutMe() {
   return (
     <>
-        <section className="container flex items-center">
-        <img src={profilePhoto} alt="profile-photo" className="w-[300px] h-[300px] rounded-full m-[50px]"/>
+      <section className="container flex items-center">
+        <img
+          src={profilePhoto}
+          alt="profile-photo"
+          className="w-[300px] h-[300px] rounded-full m-[50px]"
+        />
         <div>
-            <h2 className="text-4xl font-bold m-4">About Me</h2>
-            <p>Hi I am a full stack web developer!!!</p>
+          <h2 className="text-4xl font-bold m-4 text-center">About Me</h2>
+          <p className="font-serif text-xl mx-[90px] tracking-wide">
+            Hello! I’m Veysel Arslan, a 26-year-old full-stack web developer
+            currently residing in Chicago, USA. Originally from Turkey.
+            <br />
+            <br />
+            <p>
+              I embarked on a journey from a Civil Engineering background to the
+              dynamic world of web development. My love for coding has driven me
+              to explore and create within this exciting field.
+            </p>
+            <br />
+            I specialize in building innovative solutions and have a particular
+            passion for artificial intelligence. Over the years, I've developed
+            several projects that reflect my commitment to learning and growth.
+            This portfolio is a glimpse into my work and the projects I'm proud
+            to share. Feel free to explore my creations and delve into the
+            source code on my GitHub.
+            <br />
+            <br />
+            <p>Thank you for visiting!</p>
+          </p>
         </div>
-        </section>
-        <Outlet />
+      </section>
+      <Outlet />
     </>
   );
-};
+}
 
-export default AboutMe
+export default AboutMe;
