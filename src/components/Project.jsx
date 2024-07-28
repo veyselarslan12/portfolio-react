ec1fba7b908febb1ed0b04f99a5a790e59105d48
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-07-27 22:18:00 -0500

    imgSrc fixed

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -8,20 +8,20 @@ function Projects() {
         <div key={index} className="card bg-base-100 w-96 shadow-xl">
           <figure className="px-10 pt-10">
             <img
-              src={project.imageSource}
+              src={project.imageSrc}
               alt={project.projectName}
-              className="rounded-xl"
+              className="rounded-xl "
             />
           </figure>
           <div className="card-body items-center text-center">
             <h2 className="card-title">{project.projectName}</h2>
             <div className="card-actions">
               <a href={project.githubUrl} className="btn btn-primary" target="_blank" rel="noopener noreferrer">
-                View on GitHub
+                GitHub Repo
               </a>
               {project.depUrl && (
                 <a href={project.depUrl} className="btn btn-secondary" target="_blank" rel="noopener noreferrer">
-                  View Live
+                  Application Page
                 </a>
               )}
             </div>
