b5120807cf4b5b98f351214896334345923b9ce7
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-07-28 23:06:28 -0500

    footer fixed

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,14 +1,17 @@
 import { NavLink } from 'react-router-dom'
 
 function Nav() {
+
+    const getActiveClass = ({ isActive }) => isActive ? 'btn-primary' : 'btn-error'
+
     return (
         <>
             <nav className='mt-3'>
                 <div className='container mx-auto flex justify-end'>
-                <NavLink to='/' className='btn btn-primary text-lg mx-1 transition-transform transform hover:scale-105 hover:shadow-2xl'>About Me</NavLink>
-                <NavLink to='/portfolio' className='btn btn-primary text-lg mx-1 transition-transform transform hover:scale-105 hover:shadow-2xl'>Portfolio</NavLink>
-                <NavLink to='/contact' className='btn btn-primary text-lg mx-1 transition-transform transform hover:scale-105 hover:shadow-2xl'>Contact Me</NavLink>
-                <NavLink to='/resume' className='btn btn-primary text-lg mx-1 transition-transform transform hover:scale-105 hover:shadow-2xl'>Resume</NavLink>
+                <NavLink to='/' className={({ isActive }) => `btn text-lg mx-1 transition-transform transform hover:scale-105 hover:shadow-2xl ${getActiveClass({ isActive })}`}>About Me</NavLink>
+                <NavLink to='/portfolio' className={({ isActive }) => `btn text-lg mx-1 transition-transform transform hover:scale-105 hover:shadow-2xl ${getActiveClass({ isActive })}`}>Portfolio</NavLink>
+                <NavLink to='/contact' className={({ isActive }) => `btn text-lg mx-1 transition-transform transform hover:scale-105 hover:shadow-2xl ${getActiveClass({ isActive })}`}>Contact Me</NavLink>
+                <NavLink to='/resume' className={({ isActive }) => `btn text-lg mx-1 transition-transform transform hover:scale-105 hover:shadow-2xl ${getActiveClass({ isActive })}`}>Resume</NavLink>
                 </div>
             </nav>
         </>
